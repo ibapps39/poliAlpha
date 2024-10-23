f72b6d86a1437cce68163fe8cbc3a381e5f050ed
Ian Brown <ibapps39@gmail.com>
2024-10-22 19:53:51 -0600

    Background images and new navigation style

--- poliAlpha/Views/MapView.swift
+++ poliAlpha/Views/MapView.swift
@@ -11,43 +11,50 @@ struct MapView: View {
     @ObservedObject var gvm: GameViewModel
     
     var body: some View {
-        VStack {
-            // Display rectangles for all states in a 2x2 grid
+        ZStack {
+            Image("png_image2_politica")
+                .resizable()
+                //.scaledToFill()
+                .ignoresSafeArea(.all)
             VStack {
-                HStack {
-                    ForEach(mvm.mm.states.prefix(2), id: \.name) { state in
-                        Rectangle()
-                            .fill(state.color) // Set the rectangle's color
-                            .frame(width: 100, height: 100)
+                // Display rectangles for all states in a 2x2 grid
+                VStack {
+                    HStack {
+                        ForEach(mvm.mm.states.prefix(2), id: \.name) { state in
+                            Rectangle()
+                                .fill(state.color) // Set the rectangle's color
+                                .frame(width: 100, height: 100)
+                        }
                     }
-                }
-                HStack {
-                    ForEach(mvm.mm.states.dropFirst(2), id: \.name) { state in
-                        Rectangle()
-                            .fill(state.color) // Set the rectangle's color
-                            .frame(width: 100, height: 100)
+                    HStack {
+                        ForEach(mvm.mm.states.dropFirst(2), id: \.name) { state in
+                            Rectangle()
+                                .fill(state.color) // Set the rectangle's color
+                                .frame(width: 100, height: 100)
+                        }
                     }
-                }
-            }
-            
-            VStack {
-                // Safely unwrap highestState
-                if let highestState = gvm.highestState {
-                    Text("Highest Influence State:\n \(highestState.name) - \(highestState.baseColor.description), Influence: \(String(format: "%.2f", highestState.influence))")
-                        .font(.headline)
-                        .padding()
-                } else {
-                    Text("No state with influence yet.")
-                        .font(.headline)
-                        .padding()
                 }
                 
-                ForEach(mvm.mm.states, id: \.name) { state in
-                    Text("\(state.name) (\(state.baseColor)):\n Influence: \(String(format: "%.2f", state.influence))")
+                VStack {
+                    // Safely unwrap highestState
+                    if let highestState = gvm.highestState {
+                        Text("Highest Influence State:\n \(highestState.name) - \(highestState.baseColor.description), Influence: \(String(format: "%.2f", highestState.influence))")
+                            .font(.headline)
+                            .padding()
+                    } else {
+                        Text("No state with influence yet.")
+                            .font(.headline)
+                            .padding()
+                    }
+                    
+                    ForEach(mvm.mm.states, id: \.name) { state in
+                        Text("\(state.name) (\(state.baseColor)):\n Influence: \(String(format: "%.2f", state.influence))")
+                    }
                 }
             }
+            .padding()
         }
-        .padding()
+        
     }
 }
 
